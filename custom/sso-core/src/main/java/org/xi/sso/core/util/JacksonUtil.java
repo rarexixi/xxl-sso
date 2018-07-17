b638f5594ef9ba0874fa9fd06f8ff827af181aa2
xi <rarexixi@gmail.com>
2018-07-17 15:35:27 +0800

    修改json方法

--- custom/sso-core/src/main/java/org/xi/sso/core/util/JacksonUtil.java
+++ custom/sso-core/src/main/java/org/xi/sso/core/util/JacksonUtil.java
@@ -2,6 +2,7 @@ package org.xi.sso.core.util;
 
 import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
@@ -31,6 +32,19 @@ public class JacksonUtil {
     public static <T> T parse(String json, Class<T> clazz) {
         try {
             return objectMapper.readValue(json, clazz);
+        } catch (JsonParseException e) {
+            logger.error(e.getMessage(), e);
+        } catch (JsonMappingException e) {
+            logger.error(e.getMessage(), e);
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
+        }
+        return null;
+    }
+
+    public static <T> T parse(String jsonStr, TypeReference<T> reference) {
+        try {
+            return objectMapper.readValue(jsonStr, reference);
         } catch (JsonParseException e) {
             logger.error(e.getMessage(), e);
         } catch (JsonMappingException e) {
